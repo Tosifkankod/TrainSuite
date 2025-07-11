1beadd616b4c2e241f6afa5223b03ee5f361e082
Tosif Kankod <73471650+Tosifkankod@users.noreply.github.com>
2025-07-11 19:37:10 +0530

    feat: Setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import parser from '@typescript-eslint/parser'
+
+export default tseslint.config({
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+        parser,
+        parserOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            project: ['./tsconfig.json', './src/client/tsconfig.json', './src/server/tsconfig.json'],
+            tsconfigRootDir: import.meta.dirname
+        }
+    },
+    plugins: {
+        '@typescript-eslint': tseslint.plugin
+    },
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked, eslintConfigPrettier],
+    rules: {
+        'no-console': 'warn',
+        'no-useless-catch': 'off',
+        quotes: ['error', 'single', { allowTemplateLiterals: true }]
+    }
+})
