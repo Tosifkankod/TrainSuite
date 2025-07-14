24619075e15e6cd76f5d66f6118ea57a6a31d5fc
Tosif Kankod <73471650+Tosifkankod@users.noreply.github.com>
2025-07-14 23:01:13 +0530

    fix: Removed helloworld

--- src/client/src/app/page.tsx
+++ src/client/src/app/page.tsx
@@ -2,9 +2,6 @@ export default function Home() {
   return (
     <div className="">
       <h1 className="border-black border-2">Hello World</h1>
-      <h1 className="border-black border-2">Hello World</h1>
-      <h1 className="border-black border-2">Hello World</h1>
-      <h1 className="border-black border-2">Hello World</h1>
     </div>
   );
 }
